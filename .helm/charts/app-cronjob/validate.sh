6586ba6de596f484ef75c0c103cbb2b57d8a5729
Andru Cherny <wiroatom@gmail.com>
2024-08-18 10:40:24 +0300

    fix helm deps

--- .helm/charts/app-cronjob/validate.sh
+++ .helm/charts/app-cronjob/validate.sh
@@ -1,4 +1,4 @@
-#helm dependency update
+helm dependency update
 #helm dependency build
 helm template --debug --dry-run -f ci/default-values.yaml app .
 #helm install --debug --dry-run -f ci/default-values.yaml app .
