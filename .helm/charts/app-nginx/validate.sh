f3208af85b5e4d1a6ae4ac8be71ca61c49bbec8b
Andru Cherny <wiroatom@gmail.com>
2025-08-30 11:33:37 +0300

    Add templating utilities and support for sidecars
    Introduced new templates for rendering and merging values with support for scope, enhancing flexibility and reuse. Added sidecar container configuration and updated values, Chart, and deployment files to support customizable sidecars and service ports. Updated dependencies to the latest versions for improved compatibility and functionality.

--- .helm/charts/app-nginx/validate.sh
+++ .helm/charts/app-nginx/validate.sh
@@ -1,4 +1,4 @@
 #helm dependency update
 #helm dependency build
-helm upgrade --install --wait --debug -f ci/test.yaml app .
-#helm template --debug -f ci/default-values.yaml .
+#helm upgrade --install --wait --debug -f ci/test.yaml app .
+helm template --debug -f ci/default-values.yaml .
