f886a38d150d13e49626b0008cfcb170929161a0
Andru Cherny <wiroatom@gmail.com>
2025-08-30 11:32:36 +0300

    Update dependencies and improve Helm chart configuration
    Added `requirements.lock` file to track dependency versions. Updated checksum annotations for consistency and enhanced default version handling in templates. Minor changes include un-commenting parts of `validate.sh` and bumping the chart version to 1.0.6.

--- .helm/charts/app-job/validate.sh
+++ .helm/charts/app-job/validate.sh
@@ -1,11 +1,11 @@
-helm dependency update
+#helm dependency update
 #helm dependency build
 #helm install --debug --dry-run \
-#helm template --debug \
-#    -f ../../apps/job-migration.yaml \
-#    --set job.id=${CI_JOB_ID} \
-#    \
-#    --set extraEnvVarsSecrets[0].name=2345-generic \
-#    --set extraEnvVarsSecrets[0].checksum=yes \
-#    app .
+helm template --debug \
+    -f ../../apps/job-migration.yaml \
+    --set job.id=${CI_JOB_ID} \
+    \
+    --set extraEnvVarsSecrets[0].name=2345-generic \
+    --set extraEnvVarsSecrets[0].checksum=yes \
+    app .
 #helm template --debug -f ci/default-values.yaml .
